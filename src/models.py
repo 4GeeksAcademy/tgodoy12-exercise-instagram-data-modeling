92825c5a7ef6a9c57f390ecdc3e99af6011657b9
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-24 14:24:19 +0000

    classes and relationships created

--- src/models.py
+++ src/models.py
@@ -7,27 +7,68 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
+
+
+# My code below
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), nullable=False)
+    comments = relationship('Comments', backref='user', lazy=True)
+    posts = relationship('Posts', backref='user', lazy=True)
+    followers = relationship('Followers', backref='user', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Comments(Base):
+    __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Posts(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    comments = relationship('Comments', backref='posts', lazy=True)
+    posts = relationship('Media', backref='posts', lazy=True)
 
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
